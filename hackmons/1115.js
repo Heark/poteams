29bf40b59b2021b2ff070f26c9c9e46e2028f975
Heark <hearkofsorcerey@gmail.com>
2014-09-12 11:41:58 -0400

    Create 1115.js

---
+++ hackmons/1115.js
@@ -0,0 +1,56 @@
+Ampharos-Mega @ Leftovers
+Ability: Speed Boost
+EVs: 4 HP / 252 SpA / 252 Spe
+Modest Nature
+- Parabolic Charge
+- Toxic
+- King's Shield
+- Tail Glow
+
+Mewtwo-Mega-Y @ Leftovers
+Ability: Magic Bounce
+Shiny: Yes
+EVs: 4 HP / 252 SpA / 252 Spe
+Timid Nature
+- Focus Blast
+- Defog
+- Haze
+- Psystrike
+
+Scizor-Mega @ SilverPowder
+Ability: Flash Fire
+Shiny: Yes
+EVs: 252 HP / 252 Atk / 4 SpD
+Adamant Nature
+- X-Scissor
+- Meteor Mash
+- Bullet Punch
+- Iron Head
+
+Rotom-Wash @ Choice Specs
+Ability: Speed Boost
+EVs: 252 SpA / 4 SpD / 252 Spe
+Modest Nature
+- Hydro Pump
+- Parabolic Charge
+- Volt Switch
+- Thunder
+
+Lugia @ Leftovers
+Ability: Prankster
+EVs: 248 HP / 8 SpD / 252 Spe
+Timid Nature
+- Spore
+- Substitute
+- Stealth Rock
+- Will-O-Wisp
+
+Blissey (F) @ Leftovers
+Ability: Imposter
+EVs: 252 HP / 252 Def / 4 SpA
+Bold Nature
+- Transform
+- Soft-Boiled
+- Copycat
+- Moonblast
+
